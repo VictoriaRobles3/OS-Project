4af2e46d8d440c58a5d7155173026a01d77a5956
VictoriaRobles3 <traroblx@gmail.com>
2025-03-15 10:18:17 -0400

    Modify ReadFile to add the processes in a list instead of printing it in the terminal.

--- ReadFile.java
+++ ReadFile.java
@@ -1,13 +1,29 @@
 import java.io.*;
+import java.util.*;
 
 class ReadFile {
 
-    public void readFile(String fileName){
-        if(fileName.contains(".txt")){
+    public List<Process> readFile(String fileName){
+        List<Process> processList = new ArrayList<>();
+
+        if(fileName.endsWith(".txt")){
             try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
                 String line;
+                br.readLine();
                 while((line = br.readLine()) != null){
-                    System.out.println(line);
+                    String[] fields = line.trim().split("\\s+");
+                    if(fields.length == 4){
+                        int pid = Integer.parseInt(fields[0]);
+                        int arrival_time = Integer.parseInt(fields[1]);
+                        int burst_time = Integer.parseInt(fields[2]);
+                        int priority = Integer.parseInt(fields[3]);
+
+                        Process process = new Process(pid, arrival_time, burst_time, priority);
+                        processList.add(process);
+                    } 
+                    else{
+                        System.err.println("Invalid format: " + line);
+                    }
                 }
             } catch (IOException e){
                 e.printStackTrace();
@@ -16,9 +32,14 @@ class ReadFile {
         else{
             System.err.println("Cannot read file. File must be in txt format.");
         }
+        return processList;
     }
     public static void main (String[] args){
         ReadFile read1 = new ReadFile();
-        read1.readFile("processes.txt");
+        List<Process> processes = read1.readFile("processes.txt");
+
+        for(Process p: processes){
+            System.out.println(p);
+        }
     }
 }
